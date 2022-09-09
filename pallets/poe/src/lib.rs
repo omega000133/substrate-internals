e672e166209afc836f4b40825525ee7723cfcafb
omega <hanzhang.tw@gmail.com>
2022-09-09 18:30:51 +0800

    feat: add poe pallet

---
+++ pallets/poe/src/lib.rs
@@ -0,0 +1,177 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://docs.substrate.io/v3/runtime/frame>
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	// Declaration of the Pallet type
+	// This is a placeholder to implement traits and methods.
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	// Runtime Configuration Trait
+	// All types and constants go here.
+	// Use #[pallet::constant] and #[pallet::extra_constants]
+	// to pass in values to metadata.
+	// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		/// The maximum length a proof may be.
+		#[pallet::constant]
+		type MaxProofLength: Get<u32>;
+	}
+
+	// Runtime Storage
+	// Use to declare storage items.
+	// The pallet's runtime storage items.
+	// https://docs.substrate.io/v3/runtime/storage
+	// Learn more about declaring storage items:
+	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
+	#[pallet::storage]
+	#[pallet::getter(fn proofs)]
+	pub(super) type Proofs<T: Config> = StorageMap<
+		_,
+		Blake2_128Concat,
+		BoundedVec<u8, T::MaxProofLength>,
+		(T::AccountId, T::BlockNumber),
+		OptionQuery,
+	>;
+
+	// Hooks
+	// Define some logic that should be executed
+	// regularly in some context, for e.g. on_initialize.
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
+	// Runtime Events
+	// Can stringify event types to metadata.
+	// Pallets use events to inform users when important changes are made.
+	// https://docs.substrate.io/v3/runtime/events-and-errors
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Event documentation should end with an array that provides descriptive names for event
+		/// Event emitted when a proof has been claimed. [who, claim]
+		ClaimCreated(T::AccountId, BoundedVec<u8, T::MaxProofLength>),
+		/// Event emitted when a claim is revoked by the owner. [who, claim]
+		ClaimRevoked(T::AccountId, BoundedVec<u8, T::MaxProofLength>),
+		/// Event emitted when a claim is transferred by the owner. [from_who, to_who, claim]
+		ClaimTransferred(T::AccountId, T::AccountId, BoundedVec<u8, T::MaxProofLength>),
+	}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		/// The proof has already been claimed.
+		ProofAlreadyClaimed,
+		/// The proof does not exist, so it cannot be revoked.
+		NoSuchProof,
+		/// The proof is claimed by another account, so caller can't revoke it.
+		NotProofOwner,
+	}
+
+	// Extrinsics
+	// Functions that are callable from outside the runtime.
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(1_000)]
+		pub fn create_claim(
+			origin: OriginFor<T>,
+			proof: BoundedVec<u8, T::MaxProofLength>,
+		) -> DispatchResultWithPostInfo {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let sender = ensure_signed(origin)?;
+
+			// Verify that the specified proof has not already been claimed.
+			ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+
+			// Get the block number from the FRAME System pallet.
+			let current_block = <frame_system::Pallet<T>>::block_number();
+
+			// Store the proof with the sender and block number.
+			Proofs::<T>::insert(&proof, (&sender, current_block));
+
+			// Emit an event that the claim was created.
+			Self::deposit_event(Event::ClaimCreated(sender, proof));
+
+			Ok(().into())
+		}
+
+		#[pallet::weight(10_000)]
+		pub fn revoke_claim(
+			origin: OriginFor<T>,
+			proof: BoundedVec<u8, T::MaxProofLength>,
+		) -> DispatchResultWithPostInfo {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let sender = ensure_signed(origin)?;
+
+			// Verify that the specified proof has been claimed.
+			// Get owner of the claim.
+			let (owner, _) = Proofs::<T>::get(&proof).ok_or(Error::<T>::NoSuchProof)?;
+
+			// Verify that sender of the current call is the claim owner.
+			ensure!(sender == owner, Error::<T>::NotProofOwner);
+
+			// Remove claim from storage.
+			Proofs::<T>::remove(&proof);
+
+			// Emit an event that the claim was erased.
+			Self::deposit_event(Event::ClaimRevoked(sender, proof));
+
+			Ok(().into())
+		}
+
+		#[pallet::weight(10_000)]
+		pub fn transfer_claim(
+			origin: OriginFor<T>,
+			to: T::AccountId,
+			proof: BoundedVec<u8, T::MaxProofLength>,
+		) -> DispatchResultWithPostInfo {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let sender = ensure_signed(origin)?;
+
+			// Verify that the specified proof has been claimed.
+			// Get owner of the claim.
+			let (owner, block_number) = Proofs::<T>::get(&proof).ok_or(Error::<T>::NoSuchProof)?;
+
+			// Verify that sender of the current call is the claim owner.
+			ensure!(sender == owner, Error::<T>::NotProofOwner);
+
+			// Remove claim from storage.
+			Proofs::<T>::try_mutate(&proof, |v| -> DispatchResultWithPostInfo {
+				*v = Some((to.clone(), block_number));
+				Ok(().into())
+			})?;
+
+			// Emit an event that the claim was transferred.
+			Self::deposit_event(Event::ClaimTransferred(sender, to, proof));
+
+			Ok(().into())
+		}
+	}
+}
---
+++ pallets/poe/src/mock.rs
@@ -0,0 +1,78 @@
+use frame_support::{parameter_types, traits::ConstU32};
+use frame_system as system;
+use sp_core::H256;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+};
+
+use crate as pallet_poe;
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		PoeModule: pallet_poe::{Pallet, Call, Storage, Event<T>},
+	}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const SS58Prefix: u8 = 42;
+}
+
+impl system::Config for Test {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type Origin = Origin;
+	type Call = Call;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = ();
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = SS58Prefix;
+	type OnSetCode = ();
+	type MaxConsumers = ConstU32<16>;
+}
+
+parameter_types! {
+	pub const MaxProofLength: u32 = 32;
+}
+
+impl pallet_poe::Config for Test {
+	type Event = Event;
+
+	// Use the MaxProofLength from the parameter_types block.
+	type MaxProofLength = MaxProofLength;
+}
+
+// alternative method
+// impl pallet_poe::Config for Test {
+// 	type Event = Event;
+// 	type MaxProofLength = ConstU32<32>;
+// }
+
+// BUild genesis storage according to the mock runtime.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+}
